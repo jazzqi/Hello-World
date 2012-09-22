86bf5677b6d54c3e4e93939dd80215d3f4729c80
Fei <Fei@Fei-PC>
2012-09-22 12:48:33 +0800

    nothing more

--- src/com/qifei/helloworld/HelloWorld.java
+++ src/com/qifei/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.qifei.helloworld;
 
+//This is a test project
+
 public class HelloWorld {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		System.out.println("Hello World!");
 	}
 }
